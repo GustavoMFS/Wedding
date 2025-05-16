a37672cc0ec0fb0a72a2965d41e83b4b9baa39b5
Escapay-repo <gusmfscoder@gmail.com>
2025-05-16 12:28:40 -0300

    feat: gifts route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,19 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { authGuard } from './guard/auth.guard';
+import { GiftsComponent } from './pages/gifts/gifts.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
   {
     path: 'home',
     component: HomeComponent,
+    canActivate: [authGuard],
+    children: [],
+  },
+  {
+    path: 'home/presentes',
+    component: GiftsComponent,
     canActivate: [authGuard],
     children: [],
   },
