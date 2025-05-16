94f0ea7d81743a7b62c11a1040f08f2daa11b56a
Escapay-repo <gusmfscoder@gmail.com>
2025-05-16 11:51:08 -0300

    Fix: "localStorage is not defined"

--- src/app/guard/auth.guard.ts
+++ src/app/guard/auth.guard.ts
@@ -2,12 +2,16 @@ import { CanActivateFn, Router } from '@angular/router';
 import { inject } from '@angular/core';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  const validPW = localStorage.getItem('validPW');
-  if (validPW === 'true') {
-    return true;
-  } else {
-    const router = inject(Router);
-    router.navigate(['/']);
-    return false;
+  const router = inject(Router);
+
+  // Verifica se está rodando no navegador
+  if (typeof window !== 'undefined') {
+    const validPW = localStorage.getItem('validPW');
+    if (validPW === 'true') {
+      return true;
+    }
   }
+
+  router.navigate(['/']);
+  return false;
 };
