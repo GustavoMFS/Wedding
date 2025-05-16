826120ba9293af2a6be92329ac0d89486cc66d19
Escapay-repo <gusmfscoder@gmail.com>
2025-05-16 12:28:04 -0300

    fix: Nav to gifts working

--- src/app/template/header/header.component.ts
+++ src/app/template/header/header.component.ts
@@ -1,17 +1,21 @@
 import { DOCUMENT } from '@angular/common';
 import { Component, HostListener, Inject, OnInit } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss',
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
   menuOpen = false;
 
-  constructor(@Inject(DOCUMENT) private document: Document) {}
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     if (window.innerWidth >= 768) {
@@ -28,6 +32,20 @@ export class HeaderComponent implements OnInit {
   }
 
   scrollToSection(id: string) {
+    const currentUrl = this.router.url.split('?')[0];
+
+    if (currentUrl === '/home') {
+      // Se estiver na home, faz scroll direto
+      this.scrollToElement(id);
+      this.closeMenu();
+    } else {
+      // Se estiver em outra rota (ex: /home/presentes), navega para home com query param
+      this.router.navigate(['/home'], { queryParams: { scrollTo: id } });
+      this.closeMenu();
+    }
+  }
+
+  private scrollToElement(id: string) {
     const element = this.document.getElementById(id);
     const headerOffset = 80;
 
@@ -41,8 +59,6 @@ export class HeaderComponent implements OnInit {
         behavior: 'smooth',
       });
     }
-
-    this.closeMenu();
   }
 
   @HostListener('window:resize', ['$event'])
