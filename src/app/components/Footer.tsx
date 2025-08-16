1dc81b541448dd005ce10cdeab2769028afd94e9
Escapay-repo <gusmfscoder@gmail.com>
2025-08-16 17:36:42 -0300

    feat: footer

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import Link from "next/link";
+
+export default function Footer() {
+  const year = new Date().getFullYear();
+
+  return (
+    <footer className="w-full py-4 text-center border-t ">
+      <p className="text-sm text-gray-500">
+        © {year}{" "}
+        <Link
+          href="https://gusmfscoder.com.br"
+          target="_blank"
+          className="hover:underline"
+        >
+          gusmfscoder.com.br
+        </Link>{" "}
+        — Todos os direitos reservados.
+      </p>
+    </footer>
+  );
+}
