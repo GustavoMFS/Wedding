3bc3992e81bc922f6d486b69e65b29944479409a
Escapay-repo <gusmfscoder@gmail.com>
2025-09-19 19:46:41 -0300

    fix: footer position

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,9 +29,10 @@ export default function RootLayout({
     <ClerkProvider localization={ptBR}>
       <html lang="en">
         <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col`}
         >
-          {children}
+          <main className="flex-1">{children}</main>
+
           <Footer />
         </body>
       </html>
